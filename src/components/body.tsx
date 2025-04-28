65a098591463435122a6640f4df32558386571f1
myprojects <aboolfazlkordi83@gmail.com>
2025-04-28 13:28:18 +0330

    step 2

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,14 +1,62 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function HomePage() {
   return (
     <div>
-        <div className='relative w-full h-[650px]'>
-            <Image src="/img/background-img.webp" alt='background-lamiz' fill className='object-cover'/>
+      <div className="relative w-full h-[650px]">
+        <Image
+          src="/img/background-img.webp"
+          alt="background-lamiz"
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div className="w-full h-[3000px] [background-image:linear-gradient(to_bottom,_black_0%,_black_85%,_#E2E2B6_95%,_#E2E2B6_100%)]">
+        <div className="flex justify-center items-center gap-40 p-10">
+          <div className="w-[500px]">
+            <p className="text-xl text-white text-justify">
+              سرو قهوه فوق‌العاده، کیک‌ها و شیرینی‌های تازه و ارائه بهترین سرویس
+              در فضاهای زنده و به‌یادماندنی از جمله ارزش‌های ما در مجموعه‌ قهوه
+              لمیز است. همه هدف ما، تلاش ما و امید ما ارائه بهترین محصول با
+              بهترین قیمت و کیفیت است.
+            </p>
+          </div>
+          <div className="jusify-center text-center">
+            <h1 className="text-5xl text-[#ff6e1f] py-5 font-bold">
+              ما قهوه لمیز هستیم
+            </h1>
+            <h2 className="text-[#CFCFCF] text-4xl font-bold">
+              و بی نهایت عاشق کاری که انجام می‌دهیم
+            </h2>
+          </div>
         </div>
+        <div className="py-8 text-center">
+          <h2 className="text-[#555555] text-4xl">LAMIZ COFFEE</h2>
+        </div>
+        <div className="w-[1140px] bg-white grid grid-cols-3">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+          <div>10</div>
+          <div>11</div>
+          <div>12</div>
+          <div>13</div>
+          <div>14</div>
+          <div>15</div>
+          <div>16</div>
+          <div>17</div>
+          <div>18</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,15 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
+  const [openShop, setOpenShop] = useState(false);
+  const [openCall, setOpenCall] = useState(false);
+  const [openAnother, setOpenAnother] = useState(false);
   return (
     <header>
       <div className="flex flex-row items-center justify-around bg-black h-[90px] w-full px-50">
-        <div className="flex flex-row items-center space-x-4">
+        <div className="flex flex-row items-center space-x-6">
           <div>
             <a href="#">
               <Image
@@ -42,27 +46,120 @@ function Header() {
         </div>
 
         <div className="flex flex-row items-center">
-          <ul className="flex flex-row items-center space-x-6 text-white text-sm">
-            <li className=" hover:text-[#ff6e1f]">
-              <a href="#"> سایر </a>
+          <ul className="flex flex-row items-center space-x-6 text-white text-sm ">
+            <li
+              className="hover:text-[#ff6e1f] relative"
+              onMouseEnter={() => setOpenAnother(true)}
+              onMouseLeave={() => setOpenAnother(false)}
+            >
+              <div className="flex items-center gap-2 cursor-pointer hover:text-[#ff6e1f] ">
+                <Image
+                  src="/icons/icons8-down-arrow-24.png"
+                  alt="arrow down"
+                  width={32}
+                  height={32}
+                  className="invert hover:text-[#ff6e1f]"
+                />
+                <a href="#"> سایر </a>
+              </div>
+              {openAnother && (
+                <ul className="absolute top-full right-0 mt-2 w-40 rounded-md bg-white shadow-lg z-50">
+                  {[
+                    "سیاست مرجوعی و عودت",
+                    "انتقادات و پیشنهادات",
+                    "قوانین و مقررات",
+                    "قوانین",
+                    "مقررات",
+                  ].map((item) => (
+                    <li
+                      key={item}
+                      className="px-4 py-2 text-gray-700 hover:bg-gray-100 text-right cursor-pointer whitespace-nowrap"
+                    >
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </li>
-            <li className=" hover:text-[#ff6e1f]">
-              <a href="#">تماس با ما</a>
+            <li
+              className="hover:text-[#ff6e1f] relative "
+              onMouseEnter={() => setOpenCall(true)}
+              onMouseLeave={() => setOpenCall(false)}
+            >
+              <div className="flex items-center hover:text-[#ff6e1f] cursor-pointer gap-2">
+                <Image
+                  src="/icons/icons8-down-arrow-24.png"
+                  alt="arrow down"
+                  width={32}
+                  height={32}
+                  className="invert hover:text-[#ff6e1f]"
+                />
+                <a href="#">تماس با ما</a>
+              </div>
+              {openCall && (
+                <ul className="absolute top-full right-0 mt-2 w-20 rounded-md bg-white shadow-lg z-50">
+                  {["درباره ما", "تماس با ما "].map((item) => (
+                    <li
+                      key={item}
+                      className="px-4 py-2 text-gray-700 hover:bg-gray-100 text-right cursor-pointer whitespace-nowrap"
+                    >
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </li>
-            <li className=" hover:text-[#ff6e1f]">
+            <li className="hover:text-[#ff6e1f]">
               <a href="#">فرم درخواست همکاری با ما</a>
             </li>
-            <li className=" hover:text-[#ff6e1f]">
+            <li className="hover:text-[#ff6e1f]">
               <a href="#">سفارش از اسنپ فود</a>
             </li>
-            <li className=" hover:text-[#ff6e1f]">
+            <li className="hover:text-[#ff6e1f]">
               <a href="#">شعبه های قهوه لمیز</a>
             </li>
-            <li className=" hover:text-[#ff6e1f]">
+            <li className="hover:text-[#ff6e1f]">
               <a href="#">منو قهوه لمیز</a>
             </li>
-            <li className=" hover:text-[#ff6e1f] font-yekanbakh">
-              <a href="#">فروشگاه محصولات </a>
+            <li
+              className="relative"
+              onMouseEnter={() => setOpenShop(true)}
+              onMouseLeave={() => setOpenShop(false)}
+            >
+              <div className="flex items-center gap-2 cursor-pointer hover:text-[#ff6e1f]">
+                <Image
+                  src="/icons/icons8-down-arrow-24.png"
+                  alt="arrow down"
+                  width={32}
+                  height={32}
+                  className="invert"
+                />
+                <a href="#">فروشگاه محصولات</a>
+              </div>
+              {openShop && (
+                <ul className="absolute right-0 top-full mt-2 w-40 rounded-md bg-white shadow-lg z-50 ">
+                  {[
+                    "قهوه ها",
+                    "قهوه ترکیبی",
+                    "قهوه تک خاستگاه",
+                    "قهوه بدون کافئین",
+                    "قهوه ترک",
+                    "لمیزپرسو",
+                    "قهوه سرد دم نیترو",
+                    "شکلات لمیز",
+                    "طعم دهنده ها",
+                    "دم افزار ها",
+                    "سایر محصولات",
+                  ].map((item) => (
+                    <li
+                      key={item}
+                      className="px-4 py-2 text-gray-700 hover:bg-gray-100 cursor-pointer whitespace-nowrap text-right"
+                    >
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </li>
           </ul>
         </div>
