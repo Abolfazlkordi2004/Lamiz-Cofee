a455908b823abf19bf7cf53860f69a3ea164ec53
myprojects <aboolfazlkordi83@gmail.com>
2025-05-01 15:26:37 +0330

    step 15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import Home_page from "@/pages/Home_page";
 // import Page from "@/pages/shop/[id]/shopPage";
-import LamizChocolate from "@/pages/product-category/lamiz-chocolate/page";
+import Flavoring from "@/pages/product-category/flavoring/page";
+// import LamizChocolate from "@/pages/product-category/lamiz-chocolate/page";
 // import NitroCold from "@/pages/product-category/nitro-cold-brew-coffee/page";
 // import BranchPage from "@/pages/branches/branchPage";
 // import Page from "@/pages/product-category/coffee/[slug]";
@@ -8,7 +9,7 @@ import LamizChocolate from "@/pages/product-category/lamiz-chocolate/page";
 export default function Home() {
   return (
     <>
-      <LamizChocolate />
+      <Flavoring />
     </>
   );
 }
