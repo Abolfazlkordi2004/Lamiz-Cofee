1dd3b4cceab438dcc5c21e3e701a25c3b05d91a2
myprojects <aboolfazlkordi83@gmail.com>
2025-05-07 14:17:40 +0330

    step 32

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@
 // import Form from "@/pages/employmentForms/page";
 // import AboutPage from "@/pages/lamizCoffeeAbout/page";
 // import DropdownMenu from "@/components/dropDownMenu";
-import RefundPage from "@/pages/refundReturns/page";
+// import RefundPage from "@/pages/refundReturns/page";
 // import RefundReturnsPage from "@/pages/refundReturns/page";
 // import Contact from "@/pages/lamizContact/page";
 // import SnappFood from "@/pages/snappfood-branches/page";
 // import OtherProduct from "@/pages/product-category/other-product/page";
 
+import LamizCare from "@/pages/lamizcare/page";
+
 export default function Home() {
   return (
     <>
-      <RefundPage />
+      <LamizCare />
     </>
   );
 }
---
+++ src/components/dropDownInputCare.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useState, useRef, useEffect } from "react";
+
+type DropDownItem = {
+  id: number;
+  name: string;
+};
+
+type IDropDownInput = {
+  name: DropDownItem[];
+};
+
+export default function DropdownInputCare({ name }: IDropDownInput) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState<string>(""); // مقدار انتخاب‌شده
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  // بستن منو با کلیک بیرون
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        wrapperRef.current &&
+        !wrapperRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const handleSelect = (value: string) => {
+    setSelectedValue(value); // انتخاب مقدار
+    setIsOpen(false); // بستن منو
+  };
+
+  return (
+    <div ref={wrapperRef} className="relative w-[400px]">
+      <input
+        type="text"
+        value={selectedValue ?? ""}
+        className="w-full border border-gray-700  rounded px-4 py-2 focus:outline-none focus:ring focus:border-blue-500 text-right"
+        onClick={() => setIsOpen((prev) => !prev)}
+        readOnly
+      />
+      {isOpen && (
+        <div className="absolute top-full left-0 w-full mt-1 bg-white border border-gray-300 rounded shadow z-10 max-h-100 overflow-y-auto">
+          <ul className="divide-y divide-gray-400">
+            {name.map((item) => (
+              <li
+                className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-right"
+                key={item.id}
+                onClick={() => handleSelect(item.name)}
+              >
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
